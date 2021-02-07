4f3d0be25d35e1dba0f2d97e327ba6efbea1eeb8
Benjamin Munoz <elmaildebenja@gmail.com>
2021-02-06 20:30:03 -0800

    Fix: Update movie class with entity notation

--- app/src/main/java/com/example/moviesdatabase/model/Info.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moviesdatabase.model
-
-data class Info (
-val count: Int,
-val pages: Int
-)
--- app/src/main/java/com/example/moviesdatabase/model/Movie.kt
+++ app/src/main/java/com/example/moviesdatabase/model/Movie.kt
@@ -1,10 +1,14 @@
 package com.example.moviesdatabase.model
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "movies")
 data class Movie (
-val id: Int,
-val title: String,
-val poster_path: String,
-var url_image: String
+    @PrimaryKey val id: Int,
+    var title: String,
+    val poster_path: String,
+    var url_image: String,
+    val release_date: String,
+    val overview: String,
+    val vote_average: String
 )
